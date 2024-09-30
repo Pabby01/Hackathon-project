096c57e968f019f3da4e935b6e2f16a122c6653b
pabby01 <adeolalasisi6@gmail.com>
2024-09-30 08:46:48 +0100

    first commit

---
+++ assets/data/tracks.ts
@@ -0,0 +1,471 @@
+import { Track } from '../../src/types';
+
+export const tracks: Track[] = [
+  {
+    id: '2pK4MFls9zwq3T6H9BlRlg',
+    name: 'Blessings',
+    preview_url: 'https://www.kozco.com/tech/LRMonoPhase4.wav',
+    artists: [{ id: '2wY79sveU1sp5g7SokKOiI', name: 'L.A.X' }],
+    album: {
+      id: '4ksr9JngfntXHvy6vRb5yz',
+      name: 'Rasaking',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b27394cf165a31d12c05e1ffea10', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '2fOa0RLD4nOqIZ7PRDqFnr',
+    name: 'Duduke',
+    preview_url: 'https://www.kozco.com/tech/LRMonoPhase4.wav',
+    artists: [{ id: '1iN4MeUR7CQbmkZOgmzWXL', name: 'Simi' }],
+    album: {
+      id: '4I3cuSKBcfRaiXDakmRJRb',
+      name: 'Restless II',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b2738c3fa3f16a6e8d4972ba8b4e', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '7oK9VyNzrYvRFo7nQEYkWN',
+    name: 'Pana',
+    preview_url: 'https://www.kozco.com/tech/organfinale.wav',
+    artists: [{ id: '3tVQdUvClmATu1tSYqRd0I', name: 'Tekno' }],
+    album: {
+      id: '7c5XxVdjGUSL2PRm0KOv5F',
+      name: 'Afrobeat Hits',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273f2fa76849898fd8f539b16d4', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '0Nfe3hw1DCrVWIm9tcI9xY',
+    name: 'Joeboy',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [{ id: '5wbIWUzTPuTxTyG6ouQKqz', name: 'JoeBoy' }],
+    album: {
+      id: '2rO8fwPo8DppxN2aYbs6ce',
+      name: 'Somewhere Between Beauty & Magic',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273fc1ea6c68545e34bca9dfe44', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '67D9sUrmvA1Tlns0JrO25o',
+    name: 'Godly',
+    preview_url: 'https://www.kozco.com/tech/organfinale.wav',
+    artists: [{ id: '0Gzz2MLwmshtgEoxr52w79', name: 'Omah Lay' }],
+    album: {
+      id: '68i8H8pDhylcvo6pMe8gYO',
+      name: 'What Have We Done - EP',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273d9440488d640d8d5172712bb', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '6hFyHQpFwHnLxRGVsPI7LZ',
+    name: 'Cash App',
+    preview_url: 'https://www.kozco.com/tech/organfinale.wav',
+    artists: [{ id: '0kXmaup8OAZjza1Dgj75JC', name: 'Bella Shmurda' }, { id: '4ciFQZ3Fgmn4khlZ9t0vZw', name: 'Zlatan' }],
+    album: {
+      id: '4p2p8Ptl6JHwhecDZrn6sb',
+      name: 'High Tension 2.0',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b2732e4451d94c8bb4c86d6f8903', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '2TkCLTHctzIZw77BlMKde9',
+    name: 'Zoom',
+    preview_url: 'https://www.kozco.com/tech/LRMonoPhase4.wav',
+    artists: [{ id: '1tG7FfA4YGCi6nTAjdyb5a', name: 'Cheque' }],
+    album: {
+      id: '4qPNeOP5mYxxAnSHEZ0RbL',
+      name: 'Razor EP',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b27321f4ed4f631f348e8fb3e8b9', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '5lHRUCqkQhdB9qgpxK2mPM',
+    name: 'Ginger Me',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [{ id: '6XJ3hUouQfQwXfJO0MtnnG', name: 'Rema' }],
+    album: {
+      id: '1jTfjkDRt9Fo0GbylEpQCl',
+      name: 'Ginger Me',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273b59762e8f6939e1042dbfce7', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '1aWn9cBSyx85s8dCkPzwZm',
+    name: 'Somebody',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [{ id: '3V65VFz9QNI5KVt1Cw9qFg', name: 'DJ Neptune' }, { id: '2auC28zjQyVTAM0dwomS8S', name: 'Joeboy' }],
+    album: {
+      id: '5kLD1ODXbURTr0ewI2wPa6',
+      name: 'Greatness 2.0',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273d78c9ab564b233c0f3ec20e7', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '2hZ7i6qkJmXUg68BlPkhOB',
+    name: 'Infinity',
+    preview_url: 'https://www.kozco.com/tech/LRMonoPhase4.wav',
+    artists: [{ id: '4ScCswdRlyA23odg9thgIO', name: 'Olamide' }, { id: '0a19CzhuUvMB6Clyr6hmig', name: 'Omah Lay' }],
+    album: {
+      id: '5bqZyS0RBVtGuwOjbZ1o8l',
+      name: 'Carpe Diem',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273b25b0fa997204124b4046f74', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '6HgKXwZnIwh0CCMc3o9QiG',
+    name: 'Essence',
+    preview_url: 'https://www.kozco.com/tech/LRMonoPhase4.wav',
+    artists: [{ id: '3MM8mtgUaFGtzZAnj6HaOQ', name: 'Wizkid' }, { id: '46pWGuE3dSwY3bMMXGBvVS', name: 'Tems' }],
+    album: {
+      id: '0FEOdCHq82KIehBuAmErW3',
+      name: 'Made in Lagos',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273f7462f24abed3a340524b41e', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '3OZwqFPzAFh5VsILpUo34E',
+    name: 'Fem',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [{ id: '0SqffggOXsrQ2l8b3LKi2L', name: 'Davido' }],
+    album: {
+      id: '0V8xdx4OK0JsDIt1xfEyx7',
+      name: 'A Better Time',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273c1e089b34c44f383e0eabe54', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '4dVpf9jZjcORqGTLUaeYj9',
+    name: 'For You',
+    preview_url: 'https://www.kozco.com/tech/organfinale.wav',
+    artists: [{ id: '0VfcxRscgaK7qkMSW9XVOz', name: 'Teni' }, { id: '0Hs1FhITP1zKwWlCfrAyyz', name: 'Davido' }],
+    album: {
+      id: '2onZhx5QUWxMPkFfipPH4Y',
+      name: 'Wondaland',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273f9d6b317d3c50d94b125c94a', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '0Clb36q1TZDwhFH0BZgFaj',
+    name: 'Monalisa',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [{ id: '2IuZF0L5Qdk4A1ZtKk9qiY', name: 'Lojay' }, { id: '4XHaUB1OaJBJqOho80iYl3', name: 'Sarz' }],
+    album: {
+      id: '4TmSFrv44rksrs6UkHLCFP',
+      name: 'LV N ATTN',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b27351d0dbd4c1e15b0cfd3b0898', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '6dOxydVZ04SltQs4p1Xek5',
+    name: 'Running (To You)',
+    preview_url: 'https://www.kozco.com/tech/LRMonoPhase4.wav',
+    artists: [{ id: '1hb4DxRxJxGQz8WnZPlHcS', name: 'Chike' }, { id: '0FsP4zXjYBTghZ08GbcQ8s', name: 'Simi' }],
+    album: {
+      id: '5WqzqEFhP0tz1u5nHEox9l',
+      name: 'Boo of the Booless',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b2732fe492a2d06a9fe2f797fd35', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '1wzqaoHNQelxgz7xQbbSDU',
+    name: 'Soundgasm',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [{ id: '5CQQGHLvve5y8nhxYeGHN7', name: 'Rema' }],
+    album: {
+      id: '7L3qjZ9xHUxeAftJH8QdTy',
+      name: 'Soundgasm',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b2733bba0620b86a1ff5fffa1cb5', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '5aWDpYRslpYwNoRnN5t2DG',
+    name: 'Koroba',
+    preview_url: 'https://www.kozco.com/tech/LRMonoPhase4.wav',
+    artists: [{ id: '6GbCJZk5ZKAQX1AzhZSs92', name: 'Tiwa Savage' }],
+    album: {
+      id: '6BrwSZqltGKkz5tuNTg1Zq',
+      name: 'Celia',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273a04263b7c95d268ddf8ae2cf', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '4msm8nLFJz8JdNDr25qBaJ',
+    name: 'Woman',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [{ id: '4f6E5la2fFLgpoJmsd1l3L', name: 'Omah Lay' }],
+    album: {
+      id: '2wzUyj8cPg2HzZZOuGvHZi',
+      name: 'What Have We Done',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273d9440488d640d8d5172712bb', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '4BQBEkT4nXg59qP9BPr1O5',
+    name: 'Bling',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [{ id: '6LqFb1IknS1J5oyDyCvSnK', name: 'Blaqbonez' }, { id: '1LM3wvHfO62QbWh91m1V6f', name: 'Amaarae' }],
+    album: {
+      id: '5VmqHwfyVJQz3YGRZpeVD9',
+      name: 'Sex Over Love',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b2733e85f66de9a6d2d157d8ba2b', height: 640, width: 640 },
+      ],
+    },
+  },
+  {
+    id: '4RUhPGJvqhM9KcNwUvFOkO',
+    name: 'Jeje',
+    preview_url: 'https://www.kozco.com/tech/LRMonoPhase4.wav',
+    artists: [{ id: '3jwQf90kwrNt00ltxSgq36', name: 'Falz' }],
+    album: {
+      id: '6dP3HzpCkTGydEQi6SwnBQ',
+      name: 'Moral Instruction',
+      images: [
+        { url: 'https://i.scdn.co/image/ab67616d0000b273a31fb8f43a77e52c60c5ef7d', height: 640, width: 640 },
+      ],
+    },
+  },
+
+  {
+    id: '1dI7ijXnx23T1ymZLb4FaK',
+    name: 'Essence',
+    preview_url: 'https://www.kozco.com/tech/piano2.wav',
+    artists: [
+      {
+        id: '2wY79sveU1sp5g7SokKOiI',
+        name: 'WizKid',
+      },
+      {
+        id: '5yOvAmpIR7hVxiS6LsK9Aj',
+        name: 'Tems',
+      },
+    ],
+    album: {
+      id: '4Uv86qWpGTxf7fU7lG5X6F',
+      name: 'Made in Lagos',
+      images: [
+        {
+          url: 'https://i.scdn.co/image/ab67616d0000b2738a0eebed96a6dfdc7ad87c8b',
+          height: 640,
+          width: 640,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00001e028a0eebed96a6dfdc7ad87c8b',
+          height: 300,
+          width: 300,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d000048518a0eebed96a6dfdc7ad87c8b',
+          height: 64,
+          width: 64,
+        },
+      ],
+    },
+  },
+  {
+    id: '3nS9a01VvXHQriLqJYwRqG',
+    name: 'Last Last',
+    preview_url: null,
+    artists: [
+      {
+        id: '3ycxRkcZ67ALN3GQJ57Vig',
+        name: 'Burna Boy',
+      },
+    ],
+    album: {
+      id: '3M4ThdJR8hCb2FlAohqDXo',
+      name: 'Love, Damini',
+      images: [
+        {
+          url: 'https://i.scdn.co/image/ab67616d0000b273aaebff515dd6b458f1d02e2e',
+          height: 640,
+          width: 640,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00001e02aaebff515dd6b458f1d02e2e',
+          height: 300,
+          width: 300,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00004851aaebff515dd6b458f1d02e2e',
+          height: 64,
+          width: 64,
+        },
+      ],
+    },
+  },
+  {
+    id: '1IHWl5LamUGEuP4ozKQSXZ',
+    name: 'FEM',
+    preview_url: null,
+    artists: [
+      {
+        id: '2FXC3k01G6Gw61bmprjgqS',
+        name: 'Davido',
+      },
+    ],
+    album: {
+      id: '1Yox196Wrt9SzVtDqT6MWZ',
+      name: 'A Better Time',
+      images: [
+        {
+          url: 'https://i.scdn.co/image/ab67616d0000b2733c6885b26c9092e7b3f7e8fb',
+          height: 640,
+          width: 640,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00001e023c6885b26c9092e7b3f7e8fb',
+          height: 300,
+          width: 300,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d000048513c6885b26c9092e7b3f7e8fb',
+          height: 64,
+          width: 64,
+        },
+      ],
+    },
+  },
+  {
+    id: '4iJyoBOLtHqaGxP12qzhQI',
+    name: 'Soco',
+    preview_url:
+      'https://p.scdn.co/mp3-preview/8b4ea0fb47a5c8f4db5125f1ff69daf10045cfbb?cid=d3aa794943da4d63b6bf61a03812256e',
+    artists: [
+      {
+        id: '2wY79sveU1sp5g7SokKOiI',
+        name: 'WizKid',
+      },
+      {
+        id: '7y6c07pgjZvtHI9kuOIzYU',
+        name: 'Terri',
+      },
+    ],
+    album: {
+      id: '3H3r2nKWa3Yk5gt8xgmsEt',
+      name: 'Soco',
+      images: [
+        {
+          url: 'https://i.scdn.co/image/ab67616d0000b273276c5fb3984e4ec73f67a4c6',
+          height: 640,
+          width: 640,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00001e02276c5fb3984e4ec73f67a4c6',
+          height: 300,
+          width: 300,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00004851276c5fb3984e4ec73f67a4c6',
+          height: 64,
+          width: 64,
+        },
+      ],
+    },
+  },
+  {
+    id: '6KuqAtoeVzxAYOaZqYgOaF',
+    name: 'Jerusalema',
+    preview_url:
+      'https://p.scdn.co/mp3-preview/4d3b53aa5ac76080ebeae0177eb57117ddd1a898?cid=d3aa794943da4d63b6bf61a03812256e',
+    artists: [
+      {
+        id: '4kIWTP98INsDSa8h9Z8aSx',
+        name: 'Master KG',
+      },
+      {
+        id: '6Ha4aES39QiVjR0L2lwuwq',
+        name: 'Nomcebo Zikode',
+      },
+    ],
+    album: {
+      id: '0VSX71WHqyCXhB5woqvvj6',
+      name: 'Jerusalema',
+      images: [
+        {
+          url: 'https://i.scdn.co/image/ab67616d0000b273bf7e2487479c2d1595ff24d6',
+          height: 640,
+          width: 640,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00001e02bf7e2487479c2d1595ff24d6',
+          height: 300,
+          width: 300,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00004851bf7e2487479c2d1595ff24d6',
+          height: 64,
+          width: 64,
+        },
+      ],
+    },
+  },
+  {
+    id: '3S4U7OHV7gnj42VV72eSZ6',
+    name: 'Ye',
+    preview_url: null,
+    artists: [
+      {
+        id: '3ycxRkcZ67ALN3GQJ57Vig',
+        name: 'Burna Boy',
+      },
+    ],
+    album: {
+      id: '1AmRvh4ubMy3tsydKjlGrU',
+      name: 'Outside',
+      images: [
+        {
+          url: 'https://i.scdn.co/image/ab67616d0000b27330128cf33db80a0064912045',
+          height: 640,
+          width: 640,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d00001e0230128cf33db80a0064912045',
+          height: 300,
+          width: 300,
+        },
+        {
+          url: 'https://i.scdn.co/image/ab67616d0000485130128cf33db80a0064912045',
+          height: 64,
+          width: 64,
+        },
+      ],
+    },
+  },
+];
---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Link, Tabs } from 'expo-router';
+import { Pressable, View } from 'react-native';
+import { Text } from 'react-native';
+import { BottomTabBar } from '@react-navigation/bottom-tabs'
+import Colors from '@/src/constants/Colors';
+import { useColorScheme } from '@/src/components/useColorScheme';
+import { useClientOnlyValue } from '@/src/components/useClientOnlyValue';
+import Player from '@/src/components/Player';
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={24} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      }}
+      tabBar={(props) => (
+        <View>
+          <Player />
+          <BottomTabBar{...props}/>
+        </View>
+      )}
+      >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={Colors[colorScheme ?? 'light'].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: 'Search',
+          headerShown: false,
+          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
+        }}
+      />
+
+    <Tabs.Screen
+        name="favorites"
+        options={{
+          title: 'favorites',
+          tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
